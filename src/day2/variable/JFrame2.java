30eabae2793d62ade3989b912d305ba380f470ab
DESKTOP-MI9FPSP\KOSMO <junmyung2020>
2024-10-08 18:22:11 +0900

    add java class study file update.
    Learn Scanner, Class, Inheritance

---
+++ src/day2/variable/JFrame2.java
@@ -0,0 +1,15 @@
+package day2.variable;
+
+import javax.swing.*;
+
+public class JFrame2 {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+
+        String title = "카카오톡";
+
+        jFrame.setTitle(title);
+        jFrame.setSize(700, 500);
+        jFrame.setVisible(true);
+    }
+}
