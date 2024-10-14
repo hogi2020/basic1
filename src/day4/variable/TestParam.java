245cb4e20ec7f2dba3aebaf5287350f0c2a9d924
DESKTOP-MI9FPSP\KOSMO <junmyung2020>
2024-10-14 10:41:34 +0900

    README.md update.
    README.md update.

--- src/day4/variable/TestParam.java
+++ src/day4/variable/TestParam.java
@@ -25,6 +25,13 @@ public class TestParam {
         tp.effectParam(p);
         System.out.println("main i는 " + p.i);
     }
-
-
 }
+
+/*
+main의 i와 sub i가 같은 값을 출력하고 있습니다.
+main 메소드에서 인스턴스화된 파라미터를 넘겨주었습니다.
+(참고, 얕은 복사와 깊은 복사)
+
+이 예제에서는 main메소드에서 500이었지만, main에서 출력전에 effectParam() 먼저 호출함
+그 메소드 내부에서 i값을 다시 p.i=100으로 변경하였습니다.
+ */
