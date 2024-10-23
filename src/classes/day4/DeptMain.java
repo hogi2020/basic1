2aa3e86490d8190cfe703b774e8a06c577859fc0
DESKTOP-MI9FPSP\KOSMO <junmyung2020@gmail.com>
2024-10-23 18:58:33 +0900

    Update Thread or Socket file.
    Update Thread or Socket file.

--- src/classes/day4/DeptMain.java
+++ src/classes/day4/DeptMain.java
@@ -1,22 +1,27 @@
 package classes.day4;
 // 오라클에서 생성된 테이블을 자바의 클래스로 설계함.
 // 오라클에서 컬럼명은 변수로 이해해보자.
-
+// Value Object 역할을 수행하는 클래스로 설계하였습니다.
+// 하나의 클래스에 여러개의 변수를 선언할 수 있습니다.
+// 서로 관련이 있는 하나의 로우를 담을 수 있는 클래스 입니다.
+// 객체 배열을 사용할 수 있습니다. - n개의 로우도 담을 수 있습니다.
+// 단 고정형이다. 는 단점 - 한번 크기를 정하면 변경이 불가합니다.
+// Value Object의 역할을 수행하는 클래스는 List나 Map의 제네릭으로 사용할 수 있습니다.
+// 초기화 문제
 class Dept {
-    // 생성자
-    Dept() {}
 
+    private int deptno = 0;//10;              // 클래스 안에서만 접근 허용
+    private String dname = null;//"ACCOUNTING";  // String dname 선언
+    private String loc = null;//"NEW YORK";      // String loc 선언
+
+    // 생성자
     // 생성자 활용하기
     // 나는 생성자를 통해서 멤버변수의 초기화를 할 수 있습니다.
     // 나는 여러가지의 생성자를 재정의하여 상황에 따라 선택하여 호출할 수 있습니다.
     // 나는 this를 사용해야 하는 이유에 대해 말할 수 있습니다.
-    Dept(int deptno, String dname, String loc) {
-
-    }
-
-    private int deptno = 10;              // 클래스 안에서만 접근 허용
-    private String dname = "ACCOUNTING";  // String dname 선언
-    private String loc = "NEW YORK";      // String loc 선언
+    Dept() {}
+    Dept(String dname) {this.dname = dname;}
+    Dept(int deptno, String dname, String loc) {}
 
 
     // getter and setter // 생성 --> getter & setter
@@ -34,11 +39,9 @@ class Dept {
     public void setDeptno(int deptno) {
         this.deptno = deptno;
     }
-
     public void setDname(String dname) {
         this.dname = dname;
     }
-
     public void setLoc(String loc) {
         this.loc = loc;
     }
@@ -47,7 +50,7 @@ class Dept {
 
 public class DeptMain {
     public static void main(String[] args) {
-        Dept d = new Dept();
+        Dept d = new Dept("apple");
         System.out.println(d.getDeptno());
         System.out.println(d.getDname());
         System.out.println(d.getLoc());
--- src/ojmServer/multiThread/MultiServer.java
+++ src/ojmServer/multiThread/MultiServer.java
@@ -1,4 +1,90 @@
 package ojmServer.multiThread;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+
 public class MultiServer {
+    // 선언부
+    static private ArrayList<Socket> clientSockets = new ArrayList<>();
+
+    // Runnable 사용을 통한 Thread 관리
+    static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String clientName;
+
+        // 생성자 | 소켓을 받음
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        // 클라이언트 명으로 ArrayList search!
+        private Socket getClientByName(String OtherName) {
+            for (Socket socket : clientSockets) {
+                if (socket.equals(OtherName)) {
+                    return socket;
+                }
+            }
+            return null;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                // 입출력스트림 설정
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // 클라이언트 이름 연결
+                out.println("Input your NickName!");
+                this.clientName = in.readLine();
+                System.out.println(clientName + " | Connected Server!");
+
+                // 다른 클라이언트와 대화 가능
+                out.println("Please Input Other Client Name for 1:1!");
+                String OtherName = in.readLine();
+                Socket targerClient = getClientByName(OtherName);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    // 메인 스레드
+    public static void main(String[] args) {
+        MultiServer ms = new MultiServer();
+        ServerSocket ss = null;
+        Socket clientSocket = null;
+
+        // 서버사이드 소켓 생성
+        try {
+            ss = new ServerSocket(5000);
+        } catch (IOException e) {e.printStackTrace();}
+        System.out.println("Start Server in port[5000]...");
+
+        // 클라이언트 사이드 소켓 생성
+        while (true) {
+            try {
+                clientSocket = ss.accept();
+            } catch (IOException e) {e.printStackTrace();}
+            System.out.println("Connect New Client...");
+
+            // 클라이언트 소켓 리스트에 소켓 추가
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
+            ms.clientSockets.add(clientSocket);
+            new Thread(clientHandler).start();
+        }
+    }
 }
